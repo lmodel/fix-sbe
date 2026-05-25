1ef5eca0753871ebc9fa401dff4fa42c6c17e2c9
noelmcloughlin <noel.mcloughlin@gmail.com>
2026-05-25 19:06:57 +0100

    chore: minor updates

--- config.public.mk
+++ config.public.mk
@@ -27,3 +27,5 @@ LINKML_GENERATORS_OWL_ARGS=
 ## pass args to pydantic generator which isn't supported by gen-project
 ## https://github.com/linkml/linkml/issues/2537
 LINKML_GENERATORS_PYDANTIC_ARGS=
+
+LINKML_GENERATORS_JAVA_ARGS=--mergeimports
--- src/fix_sbe/datamodel/fix_sbe.py
+++ src/fix_sbe/datamodel/fix_sbe.py
@@ -1,5 +1,5 @@
 # Auto generated from fix_sbe.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-05-25T14:00:11
+# Generation date: 2026-05-25T19:08:30
 # Schema: fix_sbe
 #
 # id: https://w3id.org/lmodel/fix-sbe
